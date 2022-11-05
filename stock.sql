8682e637d818e81a942fe93203f8356d5b9076f1
Basheer Haadi <bashirhaadiy@gmail.com>
2022-11-05 15:15:25 +0100

    yes

--- stock.sql
+++ stock.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 19, 2016 at 05:57 AM
--- Server version: 5.5.25a
--- PHP Version: 5.4.4
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `stock`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `brands`
---
 
 CREATE TABLE IF NOT EXISTS `brands` (
   `brand_id` int(11) NOT NULL AUTO_INCREMENT,
